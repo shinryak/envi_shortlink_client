9c8724f0ef96f7b939a5473a1503ae07a95dfe12
shinryak <admin@shinryak.com>
2021-04-28 23:14:40 +0700

    add user's link list query feature

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info" sticky>
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand to="/">NavBar</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item to="/user/my-link">Link</b-nav-item>
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
---
+++ pages/user/my-link.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-12 col-md-8">
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+        { key: 'query', label: 'Query' },
+        { key: 'redirect', label: 'Link' },
+      ],
+      items: [],
+    }
+  },
+  mounted() {
+    window.console.log('sending')
+    this.$axios.$get('/user-profile/my-link').then((res) => {
+      this.items = res
+    })
+  },
+}
+</script>
