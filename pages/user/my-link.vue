8f622b08a416a14bd4b49fcba1d861e0be425700
shinryak <admin@shinryak.com>
2021-09-02 19:54:13 +0700

    new features: delete and restore link

--- pages/user/my-link.vue
+++ pages/user/my-link.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="col-12 col-md-8">
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <b-table striped hover :items="items" :fields="fields">
+      <template #cell(action)="data">
+        <b-button
+          v-if="data.item.isDeleted != true"
+          variant="danger"
+          @click="deleteLink(data.index)"
+        >
+          Delete
+        </b-button>
+        <b-button
+          v-if="data.item.isDeleted == true"
+          variant="success"
+          @click="restoreLink(data.index)"
+        >
+          Restore
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 <script>
@@ -11,6 +28,7 @@ export default {
         { key: 'query', label: 'Query' },
         { key: 'redirect', label: 'Link' },
         { key: 'redirectCount', label: 'Số lượt' },
+        'action',
       ],
       items: [],
     }
@@ -18,8 +36,19 @@ export default {
   mounted() {
     window.console.log('sending')
     this.$axios.$get('/userp/my-link').then((res) => {
+      // this.$set(this, 'items', res)
       this.items = res
     })
+  },
+  methods: {
+    deleteLink(index) {
+      this.$axios.$delete(`/userp/my-link/${this.items[index].query}/delete`)
+      this.items[index].isDeleted = true
+    },
+    restoreLink(index) {
+      this.$axios.$get(`/userp/my-link/${this.items[index].query}/restore`)
+      this.items[index].isDeleted = false
+    },
   },
 }
 </script>
